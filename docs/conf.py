8638f8057a471eba1622fb9c34f5b5e4bf770fd1
dzimmer999 <e12002168@student.tuwien.ac.at>
2024-05-31 11:34:04 +0200

    Updated conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,13 @@ author = 'Daniel Zimmer'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx_copybutton",      # adds a copy button to code blocks
+    "sphinx.ext.autodoc",     # include docstrings from modules
+    "sphinx.ext.autosummary", # create summaries of modules, aka API docs
+    "sphinx.ext.napoleon",    # support for Google and numpy-style docstrings
+    "sphinx_rtd_theme"
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -23,5 +29,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
